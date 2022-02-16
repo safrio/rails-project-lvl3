9a413dc453bbcf17e82596166530e42d527ab935
Ivan Safronov <ivan.safronov@mtxc.eu>
2022-02-16 15:51:28 +0500

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_082753) do
+ActiveRecord::Schema.define(version: 2022_02_16_083332) do
 
   create_table "bulletins", force: :cascade do |t|
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
