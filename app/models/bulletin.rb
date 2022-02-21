44f5e8489c5417a7ea55b544388ffe7256cf241a
Ivan Safronov <ivan.safronov@mtxc.eu>
2022-02-21 11:22:04 +0500

    add state to bulletins model

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,10 +1,47 @@
 # frozen_string_literal: true
 
 class Bulletin < ApplicationRecord
+  include AASM
+
   validates :title, :description, :image, presence: true
   validates :title, length: { minimum: 3 }
 
   belongs_to :category
   belongs_to :user
   has_one_attached :image
+
+  aasm column: :state do
+    state :draft, initial: true
+    state :under_moderation, :published, :rejected, :archived
+
+    event :moderate do
+      transitions from: :draft, to: :under_moderation
+    end
+
+    event :publish do
+      transitions from: :under_moderation, to: :published
+    end
+
+    event :archive do
+      transitions from: %w[draft published under_moderation], to: :archived
+    end
+
+    event :reject do
+      transitions from: :under_moderation, to: :rejected
+    end
+  end
+
+  def moderatable?
+    event?(:moderate)
+  end
+
+  def archivable?
+    event?(:archive)
+  end
+
+  private
+
+  def event?(event)
+    aasm.permitted_transitions.select { |t| t[:event] == event }.present?
+  end
 end
