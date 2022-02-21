8375a1e059656a47aa597c14a720fbc781c7199e
Ivan Safronov <ivan.safronov@mtxc.eu>
2022-02-21 11:21:02 +0500

    add admins routes and state actions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,24 @@ Rails.application.routes.draw do
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
     delete 'auth', to: 'auth#destroy'
 
-    resources :bulletins, except: :index
+    resources :bulletins, except: %w[index destroy] do
+      member do
+        patch :archive
+        patch :moderate
+      end
+    end
+    get :profile, to: 'profile#index'
+
+    scope 'admin', as: 'admin', module: :admin do
+      root 'bulletins#index'
+      resources :bulletins do
+        member do
+          patch :publish
+          patch :reject
+        end
+      end
+      resources :categories
+      resources :users
+    end
   end
 end
