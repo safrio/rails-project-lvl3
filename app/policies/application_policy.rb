471b5640bb3490a0e79354356573aa86b59d3b9c
Ivan Safronov <ivan.safronov@mtxc.eu>
2022-02-21 11:21:14 +0500

    add default policy

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  class Scope
+    def initialize(user, scope)
+      raise Pundit::NotAuthorizedError, 'must be logged in' unless user
+
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NotImplementedError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+
+  private
+
+  def author?
+    @record.user == @user
+  end
+end
